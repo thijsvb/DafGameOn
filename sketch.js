1193776ddff1f6b191268b206cb6c706ed068f9b
thijsvb <thijsvb@gmail.com>
2017-01-28 15:54:08 +0100

    nothing off screen

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
 
   for(var i=0; i!=100; ++i) {
     bins[i] = "";
-    for(var j=0; j!=1000; ++i) {
+    for(var j=0; j!=100; ++i) {
       bins[i] += floor(random(2));
     }
   }
