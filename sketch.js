70c3ec7db8af0c64fc5ec194170a43f80f9bbed9
thijsvb <thijsvb@gmail.com>
2017-01-28 14:34:42 +0100

    added button

--- sketch.js
+++ sketch.js
@@ -13,4 +13,12 @@ function draw() {
 
 function typing() {
   background(255, 0, 0);
+  if(pw.value() == "test") {
+    background(0, 255, 0);
+    pw.remove();
+    var link = createA("http://google.com", "Het wachtwoord zit onder de tafel");
+    var button = createButton('');
+    button.child(link);
+    button.position(width/2, height/2);
+  }
 }
