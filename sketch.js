65c652633be834b4527f8caf11823c44aad0870f
thijsvb <thijsvb@gmail.com>
2017-01-28 15:18:43 +0100

    move stuff

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup() {
 
 function draw() {
   for(var i=0; i!=things.length; ++i) {
+    things[i].move();
     things[i].show();
   }
 }
@@ -35,16 +36,23 @@ function typing() {
 function Thing() {
   this.a = createVector(random(width), random(height));
   this.b = createVector(random(width), random(height));
-  this.dy = abs(this.a.y - this.b.y) * ((this.a.x - this.b.x)/abs(this.a.x - this.b.x));
+  this.dy = function() {
+    return abs(this.a.y - this.b.y) * ((this.a.x - this.b.x)/abs(this.a.x - this.b.x));
+  }
 
   this.show = function() {
     stroke(255);
     strokeWeight(5);
     fill(255);
 
-    line(this.a.x, this.a.y, this.b.x + this.dy, this.a.y);
-    line(this.b.x + this.dy, this.a.y, this.b.x, this.b.y);
+    line(this.a.x, this.a.y, this.b.x + this.dy(), this.a.y);
+    line(this.b.x + this.dy(), this.a.y, this.b.x, this.b.y);
     ellipse(this.a.x, this.a.y, 10, 10);
     ellipse(this.b.x, this.b.y, 10, 10);
+  }
+
+  this.move = function() {
+    this.a.add(p5Vector.random2D().mult(3));
+    this.b.add(p5Vector.random2D().mult(3));
   }
 }
