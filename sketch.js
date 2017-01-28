1194813f764138ab92c99f3d11a0b970e23d826f
thijsvb <thijsvb@gmail.com>
2017-01-28 15:52:46 +0100

    nothing off screen

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var pw;
 var things = [];
 var locked = true;
 var w = 0;
+var bins = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,6 +15,13 @@ function setup() {
   stroke(255);
   strokeWeight(5);
   fill(255);
+
+  for(var i=0; i!=100; ++i) {
+    bins[i] = "";
+    for(var j=0; j!=1000; ++i) {
+      bins[i] += floor(random(2));
+    }
+  }
 
   for(var i=0; i!=10; ++i) {
     things[i] = new Thing();
@@ -29,9 +37,12 @@ function draw() {
     }
   } else {
     background(0);
+    fill(0, 255, 0);
+
+
     noStroke();
     fill(0, 255, 0, 127);
-    w+=5;
+    ++w;
     rect(width/2, height/2, w, height/3);
 
     if (w >= width) {
@@ -72,5 +83,10 @@ function Thing() {
   this.move = function() {
     this.a.add(this.va);
     this.b.add(this.vb);
+
+    this.a.x = (this.a.x+width)%width;
+    this.a.y = (this.a.y+height)%height;
+    this.b.x = (this.a.x+width)%width;
+    this.b.y = (this.a.y+height)%height;
   }
 }
