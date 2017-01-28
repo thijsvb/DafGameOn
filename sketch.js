46a64b639520e1abb0b7df7df377aa5b39178cca
thijsvb <thijsvb@gmail.com>
2017-01-28 14:21:11 +0100

    added text input

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
+var pw;
+
 function setup() {
   fullscreen(true);
   createCanvas(windowWidth, windowHeight);
+  pw = createInput('');
+  pw.pos(width/2, height/2);
+  pw.input(typing);
 }
 
 function draw() {
+  background(0);
+}
 
+function typing() {
+  background(255, 0, 0);
 }
